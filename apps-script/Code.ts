bf5d9c0bcae1b533ff980ed3405dd7eec9f443de
hiromi1630 <bigsea1630@docomo.ne.jp>
2024-08-06 15:15:49 +0900

    first commit: environment constructed

---
+++ apps-script/Code.ts
@@ -0,0 +1,3 @@
+function doGet() {
+  return HtmlService.createHtmlOutputFromFile("index.html").addMetaTag("viewport", "width=device-width, initial-scale=1").setTitle("Dashboard");
+}
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
+import { viteStaticCopy } from "vite-plugin-static-copy"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteSingleFile(),
+    viteStaticCopy({ targets: [{ src: "apps-script/*", dest: "./"}] })
+  ],
+})
